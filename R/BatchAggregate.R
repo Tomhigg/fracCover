015be3cb91eb2febc79bbee355a5e2f8be26f68e
Tom Higginbottom <T.Higginbottom@mmu.ac.uk>
2015-10-26 12:17:31 +0000

    uploading batchAggregate

---
+++ R/BatchAggregate.R
@@ -0,0 +1,36 @@
+#' Batch aggregate raster objects 
+#' 
+#'@description
+#' Takes a raster object and a vector of aggrgation values calculates resampling a writes to a folder
+#' 
+#'This function mainly exists as it seems difficult to use parallel cores to resample a raster object. Hence paralle the 
+#'different scale functions and use the outputs in seperate functions.  
+#'@param inRaster A raster object to be resampled
+#'@param scaleVector A vector of the different resampling factors to be applied
+#'@param outNames a string of folder location and stack names used for writing outputs, will have the aggregation values appended
+#'@param NoCores Number of Cores to used to parallel processing 
+#'@return raster objects written to file following resampling. 
+#'@examples
+#'  #resolutionVector  <-2:8
+#'  #outName <- "F:\\Projects\\shrub_cover_paper\\pixelSizeTests\\ras_stack"
+#'  #returns:  "F:\Projects\shrub_cover_paper\pixelSizeTests\ras_stack8.tif"  "F:\Projects\shrub_cover_paper\pixelSizeTests\ras_stack7.tif" etc 
+
+
+BatchAggregate <- function(inRaster,scaleVector, outNames, NoCores){
+  
+  cl <- makeCluster(mc <- getOption("cl.cores", NoCores))
+  registerDoParallel(cl, cores=NoCores)
+  clusterExport(cl=cl, varlist=c("inRaster","scaleVector", "outNames"),envir=environment())
+  clusterEvalQ(cl, library(raster))
+
+  foreach(i=resolutioVector)%dopar%{
+    filenam <- paste0(outNames,i,".tiff")
+    raster::aggregate(x = cropedstack,fact = i,FUN = mean, filename=filenam ,bylayer=FALSE)}
+  
+  stopCluster(cl)
+
+}
+
+
+
+
--- R/SpatialAggregationTest.R
+++ R/SpatialAggregationTest.R
@@ -5,45 +5,45 @@
 #' Test how changing a model resolution affect model performance. 
 
 
-#'@param 
+#'@param inRaster A raster object to be resamples 
 #'@param 
 #'@param 
 #'@param 
 #'@param 
 #'@return 
 
-rasterStack
-
-NoCores, list,fromVals, toVals, numSamps
-
-SA_AEAC_CRS
-
-SpatialAggregationTest  <- function(train,trainingY,testY,testX,sizes,NoCores){
-  
-  
-  
-  # parallel aggregate
-  
-rasterEngine()  
-  
-# parallel create csv
- All.data  <-  csv_batch_parallel(NoCores, list, predImageI, fromVals, toVals, numSamps)
-
- 
-# extract point values, split training and testing data
-xy.dats  <- SpatialPoints(coords = All.data[,1:2])
-crs(xy.dats)  <- SA_AEAC_CRS
-
-xy.dats <-spTransform(x = xy.dats,CRSobj =crs(predImageI)
-                      
-                      All.data.extracts  <-cbind(All.data, extract(predImageI, y = xy.dats)) 
-                      
-set.seed(3456)
-trainIndex <- createDataPartition(All.data.extracts$pc, p = .5,list = FALSE,                                                        times = 1)
-    PCtrain <- All.data.extracts[ trainIndex,]
-    PCtest  <- All.data.extracts[-trainIndex,]
-  
-  
-  
-  
-}
+# rasterStack
+# 
+# NoCores, list,fromVals, toVals, numSamps
+# 
+# SA_AEAC_CRS
+# 
+# SpatialAggregationTest  <- function(train,trainingY,testY,testX,sizes,NoCores){
+#   
+#   
+#   
+#   # parallel aggregate
+#   
+# rasterEngine()  
+#   
+# # parallel create csv
+#  All.data  <-  csv_batch_parallel(NoCores, list, predImageI, fromVals, toVals, numSamps)
+# 
+#  
+# # extract point values, split training and testing data
+# xy.dats  <- SpatialPoints(coords = All.data[,1:2])
+# crs(xy.dats)  <- SA_AEAC_CRS
+# 
+# xy.dats <-spTransform(x = xy.dats,CRSobj =crs(predImageI)
+#                       
+#                       All.data.extracts  <-cbind(All.data, extract(predImageI, y = xy.dats)) 
+#                       
+# set.seed(3456)
+# trainIndex <- createDataPartition(All.data.extracts$pc, p = .5,list = FALSE,                                                        times = 1)
+#     PCtrain <- All.data.extracts[ trainIndex,]
+#     PCtest  <- All.data.extracts[-trainIndex,]
+#   
+#   
+#   
+#   
+# }

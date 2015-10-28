2c78375dcac8c1777537d3d53736fe52b2afdff8
Tom Higginbottom <T.Higginbottom@mmu.ac.uk>
2015-10-28 14:05:29 +0000

    simple version of batch aggregate as old verion unreliable

--- R/BatchAggregate.R
+++ R/BatchAggregate.R
@@ -3,8 +3,7 @@
 #'@description
 #' Takes a raster object and a vector of aggrgation values calculates resampling a writes to a folder
 #' 
-#'This function mainly exists as it seems difficult to use parallel cores to resample a raster object. Hence paralle the 
-#'different scale functions and use the outputs in seperate functions.  
+
 #'@param inRaster A raster object to be resampled
 #'@param scaleVector A vector of the different resampling factors to be applied
 #'@param outNames a string of folder location and stack names used for writing outputs, will have the aggregation values appended
@@ -16,20 +15,12 @@
 #'  #returns:  "F:\Projects\shrub_cover_paper\pixelSizeTests\ras_stack8.tif"  "F:\Projects\shrub_cover_paper\pixelSizeTests\ras_stack7.tif" etc 
 
 
-BatchAggregate <- function(inRaster,scaleVector, outNames, NoCores){
+BatchAggregate <- function(inRaster,scaleVector, outNames){
   
-  cl <- makeCluster(mc <- getOption("cl.cores", NoCores))
-  registerDoParallel(cl, cores=NoCores)
-  clusterExport(cl=cl, varlist=c("inRaster","scaleVector", "outNames"),envir=environment())
-  clusterEvalQ(cl, library(raster))
-
-  foreach(i=scaleVector)%dopar%{
+  for(i in scaleVector) {
     filenam <- paste0(outNames,i,".tiff")
-    raster::aggregate(x = cropedstack,fact = i,FUN = mean, filename=filenam ,bylayer=FALSE)}
-  
-  stopCluster(cl)
-
-}
+    aggregate(x = inRaster,fact = i,FUN = mean, filename=filenam ,bylayer=FALSE)}
+  }
 
 
 
